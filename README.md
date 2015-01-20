161e300a23d5ac458f1e5bee6c372b624de2c2c8
Dem Pilafian <pilaf@centerkey.com>
2015-01-19 22:42:47 -0800

    Dashboard II now with AngularJS

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# web-calculator-ii
-Web Calculator II
+# Web Calculator II
+*Simple calculator to demonstrate data-driven templating with AngularJS*
+
+Operations:
+   * Add
+   * Subtract
+   * Multiply
+   * Divide
+   * Square root
+
+Color themes:
+   * Light
+   * Dark
+
+Note:
+As this is just for demonstration, there's no error checking.
---
+++ js/controllers.js
@@ -0,0 +1,28 @@
+// Dashboard II
+
+var calculatorApp = angular.module('calculatorApp', ['ngSanitize']);
+calculatorApp.controller('CalculatorCtrl', calculatorCtrl);
+function calculatorCtrl($scope) {
+   var math = {
+      add:      function(accumulator, value) { return accumulator + value; },
+      subtract: function(accumulator, value) { return accumulator - value; },
+      multiply: function(accumulator, value) { return accumulator * value; },
+      sqrt:     function(accumulator, value) { return Math.sqrt(accumulator); },
+      clear:    function(accumulator, value) { return 0; }
+      };
+   function doMath(operator, accumulator, value) {
+      $scope.accumulator = math[operator](parseFloat(accumulator), parseFloat(value));
+      console.log('Calculator:', operator, accumulator, value, '-->', $scope.accumulator);
+      }
+   $scope.altTheme = false;
+   $scope.buttons = [
+      { name: 'add',      display: '+' },
+      { name: 'subtract', display: '-' },
+      { name: 'multiply', display: '&times;' },
+      { name: 'sqrt',     display: '&radic;' },
+      { name: 'clear',    display: 'Clear' }
+      ];
+   $scope.value = 0;
+   $scope.accumulator = 0;
+   $scope.compute = doMath;
+   }
